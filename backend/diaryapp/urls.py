04c9cc6ec55baa39f3f3a4c87f576122d1d61f99
MTW <mtw@MTWui-MacBookPro.local>
2024-05-27 02:36:42 +0900

    search onkeydown fail

--- backend/diaryapp/urls.py
+++ backend/diaryapp/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('list/', UserDiaryListAPIView.as_view(), name='user_diaries'),
     path('user-diary/<int:pk>', DiaryDetailAPIView.as_view(), name='diary-detail'),
     path('create-diary', CreateDiaryAPIView.as_view(), name='create_diary'),
-    path('search-title/<str:string>', SearchDiaryTitleAPIView.as_view(), name='search_title'),
+    path('search-title/<str:string>/', SearchDiaryTitleAPIView.as_view(), name='search_title'),
     path('search-date/<str:date>', SearchDiaryDateAPIView.as_view(), name='search_date'),
     path('weather/<str:city>',CheckWeatherAPIView.as_view(),name='today_weather'),
 ]
--- frontend/vite-project/src/diary/DiaryListForm.jsx
+++ frontend/vite-project/src/diary/DiaryListForm.jsx
@@ -1,10 +1,56 @@
 import React, {useState,useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import TopNavbarForm from '../snippets/TopNavbarForm';
+import SideNavbarForm  from '../snippets/SideNavbarForm';
 import { getCookie } from '../utils';
+import './css/DiaryListForm.css';
+import {useParams} from 'react-router-dom';
 
 function DiaryListForm() {
     const [diaries, setDiary] = useState([]);
+    const [search, setSearch] = useState('');
+
+    const truncate = (str) => {
+        return str?.length < 50 ? str : str.substr(0, 49) + "...";
+    };
+    
+    const handleInputChange = (e) => {
+        const csrftoken = getCookie('csrftoken');
+        setSearch(e.target.value);
+        axios.get(`http://localhost:8000/api/diary/search-title/${search}/`,{
+            withCredentials: true,
+            headers:{
+                'X-CSRFToken': csrftoken,
+                'Content-Type': 'application/json',
+            }
+        })
+            .then(response=>{
+                console.log(response.length);
+                if (response.length==0){
+                    getList();
+                }else{                
+                    setDiary(response.data);
+                }
+            })
+            .catch(error => {
+                getList();
+            })
+    } 
+
+/*
+    const searchTitle = async () => {
+        try{
+            const response = await axios.get(`http://localhost:8000/api/diary/search-title/${search}`);
+            setDiary(response.data);
+        } catch (error){
+            setDiary("");
+        }
+    }
+   },[search]);
+}
+*/
+    
 
     const getList =  async () => {
         const csrftoken = getCookie('csrftoken');
@@ -17,6 +63,7 @@ function DiaryListForm() {
             }
         });
         setDiary(response.data);
+        console.log(response.data)
     } catch(error) {
         console.error('Error fetching the list:',error);
         }
@@ -27,24 +74,38 @@ function DiaryListForm() {
     
     console.log('test:', diaries)
     return (
-        <div>
-            <h1>일기 목록</h1>
-            <div>
-                {diaries.map((diary) => (
-                    <div key={diary.id}>
-                        <Link to={`/diary/${diary.id}`}>
-                            <div>
-                                <p>{diary.created_at.split('T')[0]}</p>
-                                <p>{diary.title}</p>
+        <div className='diary-list-container'>
+            <TopNavbarForm />
+            <div className='content-container'>
+                <SideNavbarForm />
+                <div className='content'>
+                    <input 
+                        type="text" 
+                        value={search} 
+                        onChange={handleInputChange} 
+                        placeholder="일기제목을 입력하세요" 
+                    />
+                    <div className='diary-container'>
+                        {diaries.map((diary) => (
+                            <div key={diary.id}>
+                                <Link to={`/diary/${diary.id}`}>
+                                    <div className='diary-item'>
+                                        <p>{diary.created_at.split('T')[0]}</p>
+                                        <div>
+                                            <p>{diary.title}</p>
+                                            <p>{truncate(diary.content)}</p>
+                                        </div>
+                                    </div>
+                                </Link>
                             </div>
-                        </Link>
+                        ))}
                     </div>
-                ))}
+                </div>
             </div>
         </div>
     );
 
-}
 
+}
 
 export default DiaryListForm;
