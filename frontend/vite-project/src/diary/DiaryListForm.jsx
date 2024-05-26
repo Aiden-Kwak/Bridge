86264cc2981dbd798a846490dc98b07cabd98036
MTW <mtw@MTWui-MacBookPro.local>
2024-05-26 23:13:54 +0900

    diary list

--- frontend/vite-project/src/diary/DiaryListForm.jsx
+++ frontend/vite-project/src/diary/DiaryListForm.jsx
@@ -1,9 +1,50 @@
+import React, {useState,useEffect} from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { getCookie } from '../utils';
+
 function DiaryListForm() {
+    const [diaries, setDiary] = useState([]);
+
+    const getList =  async () => {
+        const csrftoken = getCookie('csrftoken');
+        try{
+        const response = await axios.get('http://localhost:8000/api/diary/list',{
+            withCredentials: true,
+            headers:{
+                'X-CSRFToken': csrftoken,
+                'Content-Type': 'application/json',
+            }
+        });
+        setDiary(response.data);
+    } catch(error) {
+        console.error('Error fetching the list:',error);
+        }
+    };
+    useEffect(() => {
+        getList();
+    }, []);
+    
+    console.log('test:', diaries)
     return (
         <div>
-            <h1>이거 언제 다 만드나 list form</h1>
+            <h1>일기 목록</h1>
+            <div>
+                {diaries.map((diary) => (
+                    <div key={diary.id}>
+                        <Link to={`/diary/${diary.id}`}>
+                            <div>
+                                <p>{diary.created_at.split('T')[0]}</p>
+                                <p>{diary.title}</p>
+                            </div>
+                        </Link>
+                    </div>
+                ))}
+            </div>
         </div>
     );
+
 }
+
 
 export default DiaryListForm;
