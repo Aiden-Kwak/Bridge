470d6ad16badc6de2e17dc8d1cfd67c37ae3bef1
곽병혁 <aiden-kwak@gwagbyeonghyeog-ui-MacBookPro.local>
2024-05-27 23:51:36 +0900

    diaryDetail frame finish/css not yet

--- frontend/vite-project/src/diary/DiaryDetailForm.jsx
+++ frontend/vite-project/src/diary/DiaryDetailForm.jsx
@@ -7,8 +7,33 @@ import './css/DiaryDetailForm.css';
 function DiaryDetailForm(){
 
     const location=useLocation();
-    const test=location.state;
-    console.log(test);
+    const DiaryListProps=location.state;
+
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 +1 필요
+        const day = String(date.getDate()).padStart(2, '0');
+        return `${year}.${month}.${day}`;
+    };
+
+    const [date, setDate] = useState('');
+    const [content, setContent] = useState('');
+    const [gpt_advise, setGpt_advise] = useState('');
+    const [gpt_content, setGpt_content] = useState('');
+    const [gpt_recommend, setGpt_recommend] = useState('');
+    console.log(formatDate(DiaryListProps.propDiary.created_at));
+
+    useEffect(() => {
+        setDate(formatDate(DiaryListProps.propDiary.created_at));
+        setContent(DiaryListProps.propDiary.content);
+        setGpt_advise(DiaryListProps.propDiary.gpt_advise);
+        setGpt_content(DiaryListProps.propDiary.gpt_content);
+        setGpt_recommend(DiaryListProps.propDiary.gpt_recommend);
+    }, []);
+    /*
+    propDiary: content, created_at, gpt_advise, gpt_content, gpt_recommend, id, title, writer(pk)
+    */
     return (
         <div className="diary-detail-container">
             <TopNavbarForm /> 
@@ -16,7 +41,15 @@ function DiaryDetailForm(){
                 <SideNavbarForm />
                 <div className='content'>
                     <div className="diary-container">
-                        <p>diary</p>
+                        <p className='diary-date'>{date}</p>
+                        <div className='content-boxes'>
+                            <div className='original-content'>{content}</div>
+                            <div className='gpt-content'>{gpt_content}</div>
+                        </div>
+                        <div className='gpt-services'>
+                            <div className='gpt-recommend'>{gpt_recommend}</div>
+                            <div className='gpt-advise'>{gpt_advise}</div>
+                        </div>
                     </div>
                 </div>
             </div>
