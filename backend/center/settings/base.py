5c274aff5e6b23c29897600347b8ebd5f5711315
곽병혁 <aiden-kwak@gwagbyeonghyeog-ui-MacBookPro.local>
2024-05-29 22:59:59 +0900

    /etc/secret 경로 변경-클러스터 권한문제

--- backend/center/settings/base.py
+++ backend/center/settings/base.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import os
 
 def read_secret(secret_name):
-    file = open('/run/secrets/' + secret_name)
+    file = open('/etc/secrets/' + secret_name)
     secret = file.read()
     secret = secret.rstrip().lstrip()
     file.close()
