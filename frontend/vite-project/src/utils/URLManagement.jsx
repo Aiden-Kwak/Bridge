8beacf18f5af8d3730980453da6e6ca38400b504
곽병혁 <aiden-kwak@gwagbyeonghyeog-ui-MacBookPro.local>
2024-05-31 01:53:24 +0900

    에러 언제까지 나옴

--- frontend/vite-project/src/utils/URLManagement.jsx
+++ frontend/vite-project/src/utils/URLManagement.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function URLManagement() {
     const devBaseURL = 'http://localhost:8000'; // 개발
-    const prodBaseURL = 'http://computer-system-team-06.dev.mobilex.kr/'; // 프로덕션
+    const prodBaseURL = 'http://computer-system-team-06.dev.mobilex.kr'; // 프로덕션
 
     return process.env.NODE_ENV === 'development' ? devBaseURL : prodBaseURL;
 } // 이 고생을 누군가 알아줄까 집에 보내줘
